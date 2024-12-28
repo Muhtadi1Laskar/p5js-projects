56a6f8c69b8e80cc4c0a7673a5a927a07bfe72f0
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-12-29 02:34:57 +0600

    Added the JS folder

--- JS/Grid/rotatingGrid.js
+++ JS/Grid/rotatingGrid.js
@@ -1,8 +1,11 @@
 function setup() {
     createCanvas(400, 400);
+    rectMode(CENTER);
+    angleMode(DEGREES);
 }
 
 function draw() {
     background(220);
+    rotate(mouseY);
     rect(width/2, height/2, 100, 100);
 }
