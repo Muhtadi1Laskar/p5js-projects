1244f4f7700221bd4de4c960aa9bcfaf48be537f
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-01-06 20:23:29 +0600

    Added the Lorenz Generator

--- JS/Lorenz-Attractor/sketch.js
+++ JS/Lorenz-Attractor/sketch.js
@@ -1,51 +1,27 @@
-let x, y, z;
 let sigma = 10;
 let rho = 28;
 let beta = 8/3;
 let dt = 0.01;
-let points = [];
 let maxPoints = 100;
 
+let attractors = [];
+let num = 20;
+
 function setup() {
-    createCanvas(600, 600, WEBGL);
-    x = 1;
-    y = 1; 
-    z = 1;
+    createCanvas(600, 400, WEBGL);
+    for(let i = 0; i < num; i++) {
+        const initCond = i * 0.05;
+        attractors[i] = new Attractor(initCond, initCond, initCond);
+    }
 }
 
 function draw() {
     background(220);
     orbitControl();
-
-    let dx = (sigma * (y - x)) * dt;
-    let dy = (x * (rho - z) - y) * dt;
-    let dz = (x * y - beta * z) * dt;
-
-    x += dx;
-    y += dy;
-    z += dz;
-
     scale(5);
 
-    let p = createVector(x, y, z);
-    points.push(p);
-
-    if(points.length > maxPoints) {
-        points.shift();
+    for(let i = 0; i < num; i++) {
+        attractors[i].update();
+        attractors[i].display();
     }
-
-    strokeWeight(1);
-    for(let i =1; i < points.length; i++) {
-        let a = map(i, 0, points.length, 0, 255);
-        stroke(0, a);
-        
-        let prev = points[i-1];
-        let curr = points[i];
-
-        line(prev.x, prev.y, prev.z, curr.x, curr.y, curr.z);
-    }
-
-    let endpoint = points[points.length - 1];
-    strokeWeight(6);
-    point(endpoint.x, endpoint.y, endpoint.z);
 }
