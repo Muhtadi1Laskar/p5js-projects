c08f9d9029f86aae1f8341c3cea8dc8c960a0a0b
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-01-06 20:54:45 +0600

    Fixed the lorenz attractor code

--- JS/Lorenz-Attractor/sketch.js
+++ JS/Lorenz-Attractor/sketch.js
@@ -1,27 +1,46 @@
-let sigma = 10;
-let rho = 28;
-let beta = 8/3;
-let dt = 0.01;
-let maxPoints = 100;
-
-let attractors = [];
-let num = 20;
+let x = 0.01, y = 0, z = 0;
+let sigma = 10, rho = 28, beta = 8.0 / 3.0;
+let points = [];
 
 function setup() {
-    createCanvas(600, 400, WEBGL);
-    for(let i = 0; i < num; i++) {
-        const initCond = i * 0.05;
-        attractors[i] = new Attractor(initCond, initCond, initCond);
-    }
+    createCanvas(800, 800, WEBGL);
+    colorMode(HSB);
+    background(0);
 }
 
 function draw() {
-    background(220);
-    orbitControl();
-    scale(5);
+    background(0, 0.1);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
 
-    for(let i = 0; i < num; i++) {
-        attractors[i].update();
-        attractors[i].display();
+    let dt = 0.01;
+
+
+    let dx = sigma * (y - x) * dt;
+    let dy = (x * (rho - z) - y) * dt;
+    let dz = (x * y - beta * z) * dt;
+
+    x += dx;
+    y += dy;
+    z += dz;
+
+    points.push([x, y, z]);
+
+
+    scale(10);
+    noFill();
+    beginShape();
+    for (let i = 0; i < points.length; i++) {
+        let px = points[i][0];
+        let py = points[i][1];
+        let pz = points[i][2];
+        let hue = map(i, 0, points.length, 0, 255);
+        stroke(hue, 255, 255);
+        vertex(px, py, pz);
+    }
+    endShape();
+
+    if (points.length > 10000) {
+        points.shift();
     }
 }
---
+++ JS/Tic-Tac-Toe/game.js
@@ -0,0 +1,38 @@
+let board = [
+    ['X', 'O', 'O'],
+    ['O', 'X', ''],
+    ['O', '', 'X'],
+];
+let player1 = 'X';
+let player2 = 'O';
+
+function setup() {
+    createCanvas(800, 800);
+}
+
+function draw() {
+    background(220);
+    let w = width / 3;
+    let h = height / 3;
+
+    for (let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++) {
+            let x = w * i + w / 2;
+            let y = h * j + h / 2;
+            let spot = board[i][j];
+
+            textSize(32);
+            strokeWeight(4);
+            if (spot === player2) {
+                noFill();
+                ellipse(x, y, w / 2);
+            } else if (spot === player1) {
+                let xr = w / 4;
+                line(x-xr, y-xr, x+xr, y+xr);
+                line(x+xr, y-xr, x-xr, y+xr);
+            }
+        }
+    }
+}
+
+
